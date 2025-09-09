e71e0c55a2525fafcfb138ea64202a73fe05a9ce
Asociety !¡ <anonymouscuberguard@gmail.com>
2025-09-09 08:50:20 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,62 @@
+import os
+import sys
+os.system('clear')
+v="""                                                       
+       .d8b.  .d8888.  .d88b.   .o88b. d888888b d88888b d888888b db    db 
+      d8' `8b 88'  YP .8P  Y8. d8P  Y8   `88'   88'     `~~88~~' `8b  d8' 
+      88ooo88 `8bo.   88    88 8P         88    88ooooo    88     `8bd8'  
+      88~~~88   `Y8b. 88    88 8b         88    88~~~~~    88       88    
+      88   88 db   8D `8b  d8' Y8b  d8   .88.   88.        88       88    
+      YP   YP `8888Y'  `Y88P'   `Y88P' Y888888P Y88888P    YP       YP    
+      
+                                    \033[35mV 0.1\033[37m
+                                 
+         \033[31m }--------------{+} Coded By Cyber Claw404 {+}--------------{
+         
+                  }--------{+} t.me/Lucifer_uuu {+}--------{\033[37m  
+                      
+                      
+      """
+
+print(v)
+
+tool="""            
+     *\033[31m{\033[37m1\033[31m}\033[37m Web Hack/attack
+     *\033[31m{\033[37m2\033[31m}\033[37m Web Clone
+     *\033[31m{\033[37m3\033[31m}\033[37m Ghost killer
+     *\033[31m{\033[37m4\033[31m}\033[37m Social Meadia Hack
+     *\033[31m{\033[37m5\033[31m}\033[37m Post Exploitation
+     *\033[31m{\033[37m6\033[31m}\033[37m Information Gathering\n
+     *\033[31m{\033[37m99\033[31m}\033[37m EXIT Tool
+"""
+
+web =""" 
+  __          __  _      
+ \ \        / / | |     
+  \ \  /\  / /__| |__   
+   \ \/  \/ / _ \ '_ \  
+    \  /\  /  __/ |_) | 
+     \/  \/ \___|_.__/  
+                        
+                         """
+def end():
+ exit=input()
+print(tool)
+choice=input("Asociety~# ")
+if choice=="1":
+  os.system('clear')
+  print(web)
+  print("""
+     *\033[31m{\033[37m1\033[31m}\033[37m DDos
+     *\033[31m{\033[37m2\033[31m}\033[37m Port Scan
+     *\033[31m{\033[37m3\033[31m}\033[37m Bubsuite
+     *\033[31m{\033[37m4\033[31m}\033[37m Hydra\n
+     *\033[31m{\033[37m99\033[31m}\033[37m Back to main menu
+  """)
+elif choice=="99":
+  print("Bye!")
+  end()
+choice=input("Asociety~# ")
+if choice=="99":
+  print("Enter to Exit")
+  end()
